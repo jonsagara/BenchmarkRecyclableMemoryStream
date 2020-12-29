35ad50cd5524c6ab0376c8df542f3ffdcab95b46
Jon Sagara <jon@sagarasoftware.com>
2020-12-29 10:38:40 -0800

    Initial commit.

---
+++ src/BenchmarkRecyclableMemoryStream.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BenchmarkRecyclableMemoryStream.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
